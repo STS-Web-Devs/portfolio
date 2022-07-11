14eeb48e17d6bd46b035a9b905a40ee28f827099
Syed Adeeb <abidi9897401278@gmail.com>
2022-07-11 21:54:39 +0530

    first commit

--- .next/server/pages/api/hello.js
+++ .next/server/pages/api/hello.js
@@ -5,14 +5,15 @@ exports.id = 453;
 exports.ids = [453];
 exports.modules = {
 
-/***/ 641:
+/***/ 668:
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 __webpack_require__.r(__webpack_exports__);
 /* harmony export */ __webpack_require__.d(__webpack_exports__, {
 /* harmony export */   "default": () => (/* binding */ handler)
 /* harmony export */ });
-function handler(req, res) {
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+function handler() {
     res.status(200).json({
         name: "John Doe"
     });
@@ -28,7 +29,7 @@ function handler(req, res) {
 var __webpack_require__ = require("../../webpack-api-runtime.js");
 __webpack_require__.C(exports);
 var __webpack_exec__ = (moduleId) => (__webpack_require__(__webpack_require__.s = moduleId))
-var __webpack_exports__ = (__webpack_exec__(641));
+var __webpack_exports__ = (__webpack_exec__(668));
 module.exports = __webpack_exports__;
 
 })();
